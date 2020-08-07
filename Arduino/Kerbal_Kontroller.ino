cf4c8070bc5c51e24dc49adb74763e1cac3a15c1
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-08-06 21:10:45 -0600

    Finnished Pin Setup

--- Arduino/Kerbal_Kontroller.ino
+++ Arduino/Kerbal_Kontroller.ino
@@ -14,7 +14,7 @@ settings if you are following our directions
 // SAS + RCS + Mod
 
 const int SAS_switch = 2;
-const int RCS_switch = 2; // The-Bit-One(Or )
+const int RCS_switch = 2; // The-Bit-One(Or ) will never have this switch on
 const int mod_switch = 2;
 
 // Built in Action Groups
@@ -94,12 +94,56 @@ void simpitSetup(int pin){
 }
 
 void setup() {
-  pinMode(stage_button, INPUT);
-  pinMode(brakes_switch, INPUT);
   pinMode(LED_BUILTIN, OUTPUT);
 
   simpitSetup(LED_BUILTIN);
 
+  // Start input setup
+
+  // SAS + RCS + Mod
+
+  pinMode(SAS_switch, OUTPUT);
+  pinMode(RCS_switch, OUTPUT);
+  pinMode(mod_switch, OUTPUT);
+
+  // Built-in Action groups
+
+  pinMode(lights_switch, OUTPUT);
+  pinMode(brakes_switch, OUTPUT);
+  pinMode(gear_switch, OUTPUT);
+
+  // Stage
+
+  pinMode(stage_button, OUTPUT);
+
+  // Controls
+
+  pinMode(roll_right_button, OUTPUT);
+  pinMode(roll_left_button, OUTPUT);
+  pinMode(pitch_left_button, OUTPUT);
+  pinMode(pitch_right_button, OUTPUT);
+  pinMode(yaw_up_button, OUTPUT);
+  pinMode(yaw_down_button, OUTPUT);
+
+  // Warp Buttons
+
+  pinMode(WTM_button, OUTPUT);
+  pinMode(warp_up_button, OUTPUT);
+  pinMode(warp_down_button, OUTPUT);
+
+  // Quick Saves
+  
+  pinMode(quick_save_button, OUTPUT);
+  pinMode(revert_quick_save_button, OUTPUT);
+
+  // Action Groups
+
+  pinMode(action_1_button, OUTPUT);
+  pinMode(action_2_button, OUTPUT);
+  pinMode(action_3_button, OUTPUT);
+  pinMode(action_4_button, OUTPUT);
+  pinMode(action_5_button, OUTPUT);
+  pinMode(action_6_button, OUTPUT);
 
 }
 

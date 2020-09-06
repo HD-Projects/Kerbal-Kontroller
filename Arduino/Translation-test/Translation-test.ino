820e5dc5f6b11dcc2253be2ad0c900bf7d7e7b35
ad101-lab <55064247+ad101-lab@users.noreply.github.com>
2020-09-06 11:17:24 -0600

    😟Messed up on plexiglass

--- Arduino/Translation-test/Translation-test.ino
+++ Arduino/Translation-test/Translation-test.ino
@@ -1,47 +1,13 @@
- #include "KerbalSimpit.h"
-#include "KerbalSimpitMessageTypes.h"
-#include "PayloadStructs.h"
-
-rotationMessage myRotation; 
-
-KerbalSimpit mySimpit(Serial);
-
-int throttleValue;
-
 void setup() {
-    pinMode(LED_BUILTIN, OUTPUT);
+  Serial.begin(9600);
 
-    Serial.begin(115200);
-
-    digitalWrite(LED_BUILTIN, HIGH);
-
-    // This loop continually attempts to handshake with the plugin.
-    // It will keep retrying until it gets a successful handshake.
-    while (!mySimpit.init()) {
-        delay(100);
-    }
-    // Turn off the built-in LED to indicate handshaking is complete.
-    digitalWrite(LED_BUILTIN, LOW);
+  pinMode(2, INPUT);
 }
 
 void loop() {
-    myRotation.mask = 1|2|4;
+  delay(20);
 
-    myRotation.pitch = 0;
-
-    myRotation.roll = 0; // applies the X-axis value as the rotation roll value
-
-    myRotation.yaw = 0;
-
-    mySimpit.send(ROTATION_MESSAGE, myRotation);
-
-    delay(1);
-
-    throttleValue = 32767;
-
-    mySimpit.send(THROTTLE_MESSAGE, throttleValue);
-
-    delay(1);
-
-    mySimpit.update();
+  if (digitalRead(2) == HIGH) {
+    Serial.println("HI");
+  }
 }
